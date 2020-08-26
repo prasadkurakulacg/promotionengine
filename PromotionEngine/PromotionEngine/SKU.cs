1093afe0de9c82dcde4c9dab9e8a11da21471cae
prasadkurakulacg <prasadkurakulacg>
2020-08-26 16:20:52 +0530

    Added SKU model

---
+++ PromotionEngine/PromotionEngine/SKU.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class SKU
+    {
+        public string Id { get; set; }
+        public int Count { get; set; }
+
+
+        public SKU(string input)
+        {
+            var itemDetails = input.Split('*');
+            Id = itemDetails[1];
+            Count = Convert.ToInt32(itemDetails[0]);
+        }
+
+        public SKU(Tuple<string, int> input)
+        {
+            Id = input.Item1;
+            Count = input.Item2;
+        }
+
+        public SKU(string productId, int count)
+        {
+            Id = productId;
+            Count = count;
+        }
+    }
+}
