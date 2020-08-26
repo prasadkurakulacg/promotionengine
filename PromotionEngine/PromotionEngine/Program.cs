1c235ee64f190eec91ae533c292f686a53f873c5
prasadkurakulacg <prasadkurakulacg>
2020-08-26 16:37:36 +0530

    Read input values from the user.

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PromotionEngine
 {
     class Program
     {
-        static void Main(string[] args)
+        static Dictionary<string, int> cart = new Dictionary<string, int>();
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("total number of items");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the type of product:A,B,C or D along with quantity Example: 3*A");
+            for (int i = 0; i < a; i++)
+            {
+                string input = Console.ReadLine();
+                SKU product = new SKU(input);
+                if (cart.ContainsKey(product.Id))
+                    cart[product.Id] += product.Count;
+                else
+                    cart.Add(product.Id, product.Count);
+            }
+            var cartItems = (from item in cart select new SKU(item.Key, item.Value)).ToList();
+            Console.ReadLine();
         }
     }
 }
