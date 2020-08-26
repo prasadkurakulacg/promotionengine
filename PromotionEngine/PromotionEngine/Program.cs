c264e22e6b9b033d4e58f6ce2125e676289dcb40
prasadkurakulacg <prasadkurakulacg>
2020-08-26 18:43:54 +0530

    Un used code clean up

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -1,4 +1,5 @@
-﻿using PromotionEngine.Models;
+﻿using PromotionEngine.CalculationEngine;
+using PromotionEngine.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,20 +11,9 @@ namespace PromotionEngine
         static Dictionary<string, int> cart = new Dictionary<string, int>();
         public static void Main(string[] args)
         {
-            Console.WriteLine("total number of items");
-
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter the type of product:A,B,C or D along with quantity Example: 3*A");
-            for (int i = 0; i < a; i++)
-            {
-                string input = Console.ReadLine();
-                SKU product = new SKU(input);
-                if (cart.ContainsKey(product.Id))
-                    cart[product.Id] += product.Count;
-                else
-                    cart.Add(product.Id, product.Count);
-            }
-            var cartItems = (from item in cart select new SKU(item.Key, item.Value)).ToList();
+            var promotionService = new PromotionService();
+            var cartValue = 0;
+            
             Console.ReadLine();
         }
     }
