e2b7a7f71a91028928a34c8fb7d8dd22c630f26a
prasadkurakulacg <prasadkurakulacg>
2020-08-26 16:34:03 +0530

    Added calculationservice.

---
+++ PromotionEngine/PromotionEngine/CalculationEngine/ICalculationService.cs
@@ -0,0 +1,12 @@
+﻿using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.CalculationEngine
+{
+    public interface ICalculationService<T> where T : List<SKU>
+    {
+        int CalculateCart();
+    }
+}
