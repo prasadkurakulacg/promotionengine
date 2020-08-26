0a5ee31fd5e8e38cf53dda6c6a253efcc83b506d
prasadkurakulacg <prasadkurakulacg>
2020-08-26 16:30:38 +0530

    Added promotion interface

---
+++ PromotionEngine/PromotionEngine/CalculationEngine/IPromotion.cs
@@ -0,0 +1,12 @@
+﻿using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.CalculationEngine
+{
+    interface IPromotion<T> where T : List<SKU>
+    {
+        int ApplyPromotion();
+    }
+}
