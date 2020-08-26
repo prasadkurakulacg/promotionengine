74e51dd7f6670122f2ef32a075a06f66f8d5bb9c
prasadkurakulacg <prasadkurakulacg>
2020-08-26 18:35:27 +0530

    Added logic to combo promotion

--- PromotionEngine/PromotionEngine/CalculationEngine/PromotionService.cs
+++ PromotionEngine/PromotionEngine/CalculationEngine/PromotionService.cs
@@ -9,7 +9,18 @@ namespace PromotionEngine.CalculationEngine
     {
         public int ApplyComboPromotion(SKU item1, SKU item2)
         {
-            throw new NotImplementedException();
+            if (item1.Count == item2.Count)
+            {
+                return item2.Count * item1.OfferPrice;
+            }
+            else if (item1.Count > item2.Count)
+            {
+                return ((item1.Count - item2.Count) * item1.ActualPrice) + item2.Count * item2.ActualPrice;
+            }
+            else
+            {
+                return item1.Count * item1.OfferPrice + ((item2.Count - item1.Count) * item2.ActualPrice);
+            }
         }
 
         public int ApplySinglePromotion(SKU item)
